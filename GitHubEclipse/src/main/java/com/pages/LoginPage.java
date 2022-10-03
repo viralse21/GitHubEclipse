6a30815bb762dd4041750b2a2f4c7e1c13aafb9d
Viral Upadhyay <viralce@gmail.com>
2022-10-03 17:29:21 -0400

    First Commit

---
+++ GitHubEclipse/src/main/java/com/pages/LoginPage.java
@@ -0,0 +1,10 @@
+package com.pages;
+
+public class LoginPage {
+
+	
+	public void loginPage()
+	{
+		System.out.println("This is Login Page");
+	}
+}
---
+++ GitHubEclipse/src/test/java/com/testcases/LoginTest.java
@@ -0,0 +1,12 @@
+package com.testcases;
+
+public class LoginTest {
+	
+	public void loginTest()
+	{
+		System.out.println("This is Login Test");
+
+		System.out.println("This is new comment");
+	}
+
+}
